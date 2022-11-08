1cf2779866da2985b1aee99147016a7953beede5
KingOfCoders <17mkhuselimkeyiya@gmail.com>
2022-11-08 10:31:47 +0200

    Challenge Done

---
+++ code.js
@@ -0,0 +1,7 @@
+let name_prompt = prompt("Enter your name: ");
+let surname = prompt("Enter your surname");
+var salary = parseInt(prompt("Enter a salary", "0"));
+let bonus = 500 + salary;
+console.log(`Name:${name_prompt}`);
+console.log(`SurName:${surname}`);
+console.log(`Bonus:${bonus}`);
